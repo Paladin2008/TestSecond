60bc6505a043351448f1454a91e4214d864808f8
vladimir <VEMatveev@alfabank.ru>
2024-03-18 22:30:48 +0500

    Lesson16 task3

---
+++ src/main/kotlin/lessons/Task3.kt
@@ -0,0 +1,23 @@
+package lessons
+
+//Создай класс, описывающий пользователя, который хранит поля логин и пароль.
+//Пароль не должен быть виден в теле главной функции, но
+//должен быть способ проверить, что пароль введен верно. Функция валидации должна
+//принимать строку и возвращать результат проверки (и только его – без распечатки или генерации других строк).
+//Выведи информацию о релевантности пароля в консоль.
+fun main() {
+
+    val user = User()
+    val inputPass = readln()
+
+    println(user.validation(inputPass))
+}
+
+class User() {
+
+    val login = "login"
+    private val pass = "пароль"
+    fun validation(enteredPass: String): Boolean {
+        return enteredPass == pass
+    }
+}
